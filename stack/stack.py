a473fc39cba1f34c33b8935daad6a057d7d07eb7
robomantis19 <freefavors@gmail.com>
2020-05-11 18:47:52 -0400

    almost finished the stack

--- stack/stack.py
+++ stack/stack.py
@@ -12,29 +12,29 @@ return elements in Last In First Out order.
 """
 import sys
 sys.path.append("../doubly_linked_list")
-from doubly_linked_list import DoublyLinkedList, ListNode
+from doubly_linked_list import ListNode
 
-class Stack:
-    def __init__(self):
-        self.size = 0
-        self.array = []
-        #self.storage = ?
+# class Stack:
+#     def __init__(self):
+#         self.size = 0
+#         self.array = []
+#         #self.storage = ?
 
-    def __len__(self):
-        return self.size
+#     def __len__(self):
+#         return self.size
 
-    def push(self, value):
-        self.array.append(value)
-        self.size += 1
-        return self.array
+#     def push(self, value):
+#         self.array.append(value)
+#         self.size += 1
+#         return self.array
 
-    def pop(self):
-        if self.__len__() > 0: 
-            popped_item = self.array.pop()
-            self.size -= 1
-            return popped_item
-        else: 
-            return None
+#     def pop(self):
+#         if self.__len__() > 0: 
+#             popped_item = self.array.pop()
+#             self.size -= 1
+#             return popped_item
+#         else: 
+#             return None
 
 # stack = Stack1()
 # stack.push(1)
@@ -43,16 +43,27 @@ class Stack:
 # print(stack.__len__())
 # print(stack.array)
 
-class Stack2:
+class Stack:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.storage = ListNode(1)
 
     def __len__(self):
-        pass
+        return self.size
+        
 
     def push(self, value):
-        pass
-
+        # self.storage = ListNode(value)
+        self.storage.insert_after(value)
+        self.size += 1
+        return self.storage
     def pop(self):
-        pass
+        if self.size > 0: 
+            popped = self.storage.delete()
+            print(popped)
+            self.size -= 1
+            return popped
+        else: 
+            return None
+            
+        
